6d0bf7f1affb255f9a6d61b76432cad2bdaca851
V1kata <viktor.burboran@gmail.com>
2023-03-15 20:30:07 +0200

    addUser, getUser and updateUser

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,47 @@
+const { errorHandler } = require('../utils/errorHandler');
+const { getAll, createUser, getUserById, updateUserById } = require("../services/userServise");
+
 exports.getUsers = async (req, res) => {
     try {
-        
+        const data = await getAll();
+
+        res.status(200).json({ user: data });
     } catch(err) {
-        
+        errorHandler(err, res, req);
+    }
+}
+
+exports.addUser = async (req, res) => {
+    try {
+        const data = {...req.body, posters: []}
+        const user = await createUser(data);
+
+        res.status(200).json({ user });
+    } catch (err) {
+        errorHandler(err, res, req);
+    }
+}
+
+exports.getUser = async (req, res) => {
+    const { userId } = req.params;
+
+    try {
+        const user = await getUserById(userId);
+
+        res.status(200).json({ user })
+    } catch (err) {
+        errorHandler(err, res, req);
+    }
+}
+
+exports.updateUser = async (req, res) => {
+    const { userId } = req.params;
+
+    try {
+        const user = await updateUserById(userId, req.params);
+
+        res.status(200).json({ user })
+    } catch (err) {
+        errorHandler(err, res, req);
     }
 }
