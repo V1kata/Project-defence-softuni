2f926a79ad9705408a39d552566aaee38fdb287f
V1kata <viktor.burboran@gmail.com>
2023-03-15 20:29:25 +0200

    User servise
    Added findById, create and update

--- server/services/userServise.js
+++ server/services/userServise.js
@@ -3,3 +3,7 @@ const User = require('../models/User');
 exports.getAll = () => User.find();
 
 exports.getUserById = (id) => User.findById(id);
+
+exports.createUser = (data) => User.create(data);
+
+exports.updateUserById = (id, data) => User.findByIdAndUpdate(id, data);
