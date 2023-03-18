edc8fdf1cc578d745837df16e4a5fef01acf9062
V1kata <viktor.burboran@gmail.com>
2023-03-18 19:27:09 +0200

    utils for requests

---
+++ client/src/utils/bidItemUtils.js
@@ -0,0 +1,32 @@
+const host = 'http://localhost:5000';
+
+export const request = async (method, url, data) => {
+    const options = {
+        method,
+        headers: {}
+    }
+
+    if (data) {
+        options.headers['Content-type'] = 'aplication/json';
+        options.body = JSON.stringify(data);
+    }
+
+    try {
+        const response = await fetch(host + url, options);
+
+        if (response.status === 204) {
+            return response;
+        }
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            throw new Error(data.message || data.error);
+        }
+
+        return data.bidItem;
+    } catch (err) {
+        alert(err.message);
+        throw err;
+    }
+}
