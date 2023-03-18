ecfe3e41c1c070b34282a6e3ffb7ae506a305d94
V1kata <viktor.burboran@gmail.com>
2023-03-18 19:25:05 +0200

    Cross Origin header
    Now you can send requests to localhost:3000 while getting client service on localhost:5000

--- server/index.js
+++ server/index.js
@@ -7,6 +7,13 @@ const app = express();
 app.set('view engine', 'hbs');
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use(express.json())
 app.use(router);
 
