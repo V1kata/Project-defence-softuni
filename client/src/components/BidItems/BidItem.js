4d8c3849c509fd9d2acbbd34497b70dcc1a62711
V1kata <viktor.burboran@gmail.com>
2023-03-19 20:41:32 +0200

    Made the css work

--- client/src/components/BidItems/BidItem.js
+++ client/src/components/BidItems/BidItem.js
@@ -1,19 +1,17 @@
 export function BidItem({ _id, price, imageUrl, typeOfPurchase, title, description }) {
     return (
-        <section className="container" key={'pp'}>
-            <div className="auction-item">
-                <div className="info-container">
-                    <img src={imageUrl} alt={title} />
-                    <div>
-                        <h1>{description}</h1>
-                        <h2>Price: {price}$</h2>
-                        <h3>type: {typeOfPurchase}</h3>
-                    </div>
-                </div>
-                <div className="details-btn">
-                    <a href={`/details/${_id}`} className="details">Details</a>
+        <div className="auction-item">
+            <div className="info-container">
+                <img src={imageUrl} alt={title} />
+                <div>
+                    <h1>{description}</h1>
+                    <h2>Price: {price}$</h2>
+                    <h3>type: {typeOfPurchase}</h3>
                 </div>
             </div>
-        </section>
+            <div className="details-btn">
+                <a href={`/details/${_id}`} className="details">Details</a>
+            </div>
+        </div>
     )
 }
