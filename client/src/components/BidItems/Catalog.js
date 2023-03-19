da706ec47c657de49ec509048f49c021b6fcdf45
V1kata <viktor.burboran@gmail.com>
2023-03-19 20:07:46 +0200

    Made the style error

--- client/src/components/BidItems/Catalog.js
+++ client/src/components/BidItems/Catalog.js
@@ -4,7 +4,9 @@ export function Catalog({ bidItems }) {
 
     return (
         <>
+         <section className="container">
             {bidItems.map(x => <BidItem key={x._id} {...x} />)}
+         </section>
         </>
     )
 }
