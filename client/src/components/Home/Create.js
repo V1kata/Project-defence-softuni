5f4f7ccf2c6215cf44b8de0287ff15aaeadebbf8
V1kata <viktor.burboran@gmail.com>
2023-03-19 20:43:07 +0200

    Create works now

--- client/src/components/Home/Create.js
+++ client/src/components/Home/Create.js
@@ -1,30 +1,28 @@
 import { useForm } from "../../hooks/useForm";
 
-export function Create() {
-    const { formValues, onChangeHandler, onSubmit } = useForm({
+export function Create({ onSubmit }) {
+    const { formValues, onChangeHandler } = useForm({
         title: '',
-        desciption: '',
-        price: 0,
+        description: '',
+        price: '',
         imageUrl: '',
         typeOfPurchase: 'Sell'
-    }, (values) => {
-        console.log(values);
     });
 
     return (
         <section className="forms">
 
-            <form method="" onSubmit={onSubmit}>
+            <form onSubmit={(e) => onSubmit(e, 'post', '/bidItems', formValues, '/catalog')}>
                 <h3>Create</h3>
 
                 <label htmlFor="title">Title</label>
                 <input type="text" id="title" name="title" className="box" value={formValues.title} onChange={onChangeHandler} />
 
-                <label htmlFor="desciption">Desciption</label>
-                <input type="text" id="desciption" name="desciption" className="box" value={formValues.desciption} onChange={onChangeHandler} />
+                <label htmlFor="description">Description</label>
+                <input type="text" id="description" name="description" className="box" value={formValues.description} onChange={onChangeHandler} />
 
                 <label htmlFor="price">Price</label>
-                <input type="number" id="Price" name="price" className="box" value={formValues.price} onChange={onChangeHandler} />
+                <input type="number" id="price" name="price" className="box" value={formValues.price} onChange={onChangeHandler} />
 
                 <label htmlFor="imageUrl">Image</label>
                 <input type="text" id="imageUrl" name="imageUrl" className="box" value={formValues.imageUrl} onChange={onChangeHandler} />
