fc5aece0e2f5c280a66e72b654041f67f52e5a35
V1kata <viktor.burboran@gmail.com>
2023-03-22 20:29:05 +0200

    useForm update

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export const useForm = (initValues) => {
+export const useForm = (initValues, onSubmitHandler) => {
     const [formValues, setFormValues] = useState(initValues);
 
     const onChangeHandler = (e) => {
@@ -9,8 +9,20 @@ export const useForm = (initValues) => {
         setFormValues(state => ({...state, [target.name]: target.value}));
     }
 
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        onSubmitHandler(formValues);
+    }
+
+    const changeValues = (newValues) => {
+        setFormValues(newValues)
+    }
+
     return {
         formValues,
         onChangeHandler,
+        onSubmit,
+        changeValues
     }
 }
