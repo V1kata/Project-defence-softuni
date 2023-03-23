ce737d6e9ea8e6809f18cba7970d5717d0a45768
V1kata <viktor.burboran@gmail.com>
2023-03-23 21:54:11 +0200

    User profile and his posts

--- client/src/components/Home/Profile.js
+++ client/src/components/Home/Profile.js
@@ -1,24 +1,43 @@
+import { useState, useEffect, useContext } from "react";
+import { useService } from "../../hooks/useService";
+import { authServiseFactory } from "../../services/authService";
+import { AuthContext } from "../../contexts/AuthContext";
+import { ProfileItem } from "./ProfileItem";
+
 export function Profile() {
+    const [user, setUser] = useState({});
+    const { userId } = useContext(AuthContext);
+    const authService = useService(authServiseFactory);
+
+    useEffect(() => {
+        async function request() {
+            const data = await authService.getUser(userId);
+
+            setUser(data.user);
+        }
+
+        request();
+    }, []);
+
+    const name = user?.firstName + ' ' + user?.lastName;
+    const posts = user.posters?.length;
+
     return (
         <section>
             <div className="data">
                 <div className="profile-img">
-                    <img src="/images/person.png" alt="Blank profile" />
+                    <img src={user.imageUrl} alt={name} />
                 </div>
 
                 <div className="personal-data">
-                    <p>Name: Viktor Hristov</p>
-                    <p>Email: viktor.burboran@gmail.com</p>
-                    <p>2 posts</p>
+                    <p>Name: {name}</p>
+                    <p>Email: {user.email}</p>
+                    <p>{posts} posts</p>
                 </div>
             </div>
 
             <div className="auction-images">
-                <div className="photo">
-                    <a href="/details/">
-                        <img src="/images/TruckBid.jpg" alt="Truck" />
-                    </a>
-                </div>
+                {user['posters'] && user['posters'].map(x => <ProfileItem key={x._id} {...x} />)}
             </div>
         </section>
     );
